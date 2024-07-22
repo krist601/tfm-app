349ebbb4eb3abb713fd16c85d9c17dcb2178870e
Kristian Cortes <kscortesp@falabella.cl>
2024-07-22 13:37:48 -0400

    feat: few changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
+import { SummarizerComponent } from './summarizer/summarizer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SummarizerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
